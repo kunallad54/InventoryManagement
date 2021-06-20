043de8527e00f4a366e1966742f634d8228ce999
Krunal Lad <32663372+kunallad54@users.noreply.github.com>
2021-06-20 23:31:10 +0530

    Update InventoryServiceInterface.java

--- InventoryService/InventoryServiceInterface.java
+++ InventoryService/InventoryServiceInterface.java
@@ -2,9 +2,6 @@ package InventoryService;
 
 import InventoryModel.Inventory;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface InventoryServiceInterface {
 
 
@@ -14,11 +11,6 @@ public interface InventoryServiceInterface {
 
     void editInventory(String name, double price, double weight);
 
-    boolean findByName(String name);
-
-    List<Inventory> findAll();
+    void display();
 
 }
-///public abstract ArrayList<Inventory> addInventory(Inventory inventory,ArrayList<Inventory>arrayList);
-//    //public abstract ArrayList<Inventory> editInventory(Inventory inventory,ArrayList<Inventory> arrayList,String editName);
-//    //public abstract ArrayList<Inventory> deleteInventory(Inventory inventory,ArrayList<Inventory>arrayList,String deleteName);
