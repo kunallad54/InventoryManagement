e42219dd1caf28f0c23eb99c07f36c2278c46b01
Krunal Lad <32663372+kunallad54@users.noreply.github.com>
2021-06-21 00:06:21 +0530

    Update InventoryMain.java

--- InventoryController/InventoryMain.java
+++ InventoryController/InventoryMain.java
@@ -1,18 +1,28 @@
 package InventoryController;
+/**
+ * User input of inventory name, price and weight.
+ * Add it in the json array.
+ * Edit the inventory to change weight and price.
+ * Delete the inventory.
+ * Print the json array in the console.
+ * Calculate the total weight, total price and total value of the inventory stored.
+ * 
+ * @author Krunal Lad
+ * @Since 21-06-2021
+ */
 
 import InventoryModel.Inventory;
 import InventoryService.InventoryService;
 import Util.UserInputOutput;
-
-import java.util.Scanner;
 
 public class InventoryMain {
 
     public static void main(String[] args) {
 
         InventoryService inventoryService = new InventoryService();
+        boolean flag = true;
 
-        while (true) {
+        while (flag) {
             int choice = UserInputOutput.getUserChoice();
 
             switch (choice) {
@@ -24,26 +34,38 @@ public class InventoryMain {
                     String editName = UserInputOutput.getProductName();
                     double price = UserInputOutput.getProductPrice();
                     double weight = UserInputOutput.getProductWeight();
-
                     inventoryService.editInventory(editName, price, weight);
-
                     break;
                 case 3:
                     String name = UserInputOutput.getProductName();
-
                     inventoryService.deleteInventory(name);
                     break;
                 case 4:
                     inventoryService.display();
                     break;
+                case 5:
+                    System.out.println("\n" + "Total value of Inventory");
+                    inventoryService.totalValueOfInventory();
+                    break;
+                case 6:
+                    System.out.println("\n" + "Total weight of Inventory");
+                    inventoryService.totalWeightOfInventory();
+                    break;
+                case 7:
+                    System.out.println("\n" + "Total price of Inventory");
+                    inventoryService.totalPriceOfInventory();
+                    break;
+                case 8:
+                    flag = false;
+                    break;
                 default:
+                    System.out.println("Invalid Input !!!");
                     break;
             }
-            if(choice  == 5)
-                break;
         }
     }
 
+    // returns inventory object stores name,price,weight
     public static Inventory readDataFromConsole() {
         Inventory inventory = new Inventory();
         System.out.println("Add Inventory");
