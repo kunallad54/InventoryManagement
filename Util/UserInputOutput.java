01e793b9886fa05b232f204626520b83ab6f721d
Krunal Lad <32663372+kunallad54@users.noreply.github.com>
2021-06-21 00:07:29 +0530

    Update UserInputOutput.java

--- Util/UserInputOutput.java
+++ Util/UserInputOutput.java
@@ -23,11 +23,14 @@ public class UserInputOutput {
 
     public static int getUserChoice() {
 
-        System.out.println("Press 1 - Add Products ");
-        System.out.println("Press 2 - Edit Products ");
-        System.out.println("Press 3 - Delete Products ");
-        System.out.println("Press 4 - Display Products ");
-        System.out.println("Press 5 - Quit ");
+        System.out.println("Press 1 - Add Products to Inventory");
+        System.out.println("Press 2 - Edit Products of Inventory");
+        System.out.println("Press 3 - Delete Products from Inventory");
+        System.out.println("Press 4 - Display Products of Inventory");
+        System.out.println("Press 5 - To view Total Value of Inventory ");
+        System.out.println("Press 6 - To view Total Weight of Inventory ");
+        System.out.println("Press 7 - To view Total Price of Inventory ");
+        System.out.println("Press 8 - To Quit ");
 
         return scanner.nextInt();
     }
